58e76636e0bddcd6ee8c9282a91a1f75f18e4e25
selman.kaya <kayaselmann@gmail.com>
2023-11-06 01:43:30 +0300

    some refactors

---
+++ src/main/java/com/axial/modules/commons/PackageMarker.java
@@ -0,0 +1,9 @@
+package com.axial.modules.commons;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PackageMarker {
+
+}
